b6f19f28b0f588ebb3c2c4e097651439368a4b43
samihasamin <samihasamin@hotmail.com>
2025-02-15 01:16:15 -0500

    Add photos to photo gallery

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Photogallery from "./components/Photogallery/Photogallery";
+import photosData from "./data/photosData";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [photos, setPhotos] = useState(photosData);
   return (
     <>
       <Header />
+      <Photogallery photos={photos} />
       <Footer />
     </>
   );
--- src/components/Photogallery/Photogallery.jsx
+++ src/components/Photogallery/Photogallery.jsx
@@ -1,7 +1,29 @@
 import "./Photogallery.scss";
 
-function Photogallery() {
-  return <></>;
+function Photogallery({ photos }) {
+  return (
+    <>
+      <div className="snaps__gallery">
+        {photos.map((image, index) => {
+          return (
+            <div className="snaps__gallery-card" key={image.id}>
+              <div className="snaps__gallery-card-image">
+                <img src={image.photo} alt="Snaps Image" />
+                <p>{image.photographer}</p>
+              </div>
+              <div className="snaps__gallery-card-tags">
+                {image?.tags.map((tag, index) => (
+                  <div className="snaps__gallery-card-tags-tag" key={index}>
+                    {tag}
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Photogallery;
